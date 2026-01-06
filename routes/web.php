d0f45d9313a895e37290d7ac428125136279007f
rui95fer <rui95fer@gmail.com>
2026-01-06 22:05:50 +0000

    Creating the Feed Item Partial View

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,53 @@ Route::get('/', function () {
 });
 
 Route::get('/feed', function () {
-    return view('feed');
+    $feedItems = json_decode(
+        json_encode([
+            [
+                'postedDateTime' => '3h',
+                'content' => <<<srt
+                    <p>
+                        I made this! <a href="#">#myartwork</a> <a href="#">#pixl</a>
+                    </p>
+                    <img src="/images/simon-chilling.png" alt=""/>
+                srt,
+                'likeCount' => 24,
+                'replyCount' => 4,
+                'repostCount' => 2,
+                'profile' => [
+                    'avatar' => '/images/michael.png',
+                    'displayName' => 'Michael',
+                    'handle' => '@mmich_jj'
+                ]
+            ]
+        ])
+    );
+
+    return view('feed', compact('feedItems'));
 });
 
 Route::get('/profile', function () {
-    return view('profile');
+    $feedItems = json_decode(
+        json_encode([
+            [
+                'postedDateTime' => '3h',
+                'content' => <<<srt
+                    <p>
+                        I made this! <a href="#">#myartwork</a> <a href="#">#pixl</a>
+                    </p>
+                    <img src="/images/simon-chilling.png" alt=""/>
+                srt,
+                'likeCount' => 24,
+                'replyCount' => 4,
+                'repostCount' => 2,
+                'profile' => [
+                    'avatar' => '/images/michael.png',
+                    'displayName' => 'Michael',
+                    'handle' => '@mmich_jj'
+                ]
+            ]
+        ])
+    );
+
+    return view('profile', compact('feedItems'));
 });
