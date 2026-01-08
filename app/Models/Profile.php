c80906c62988c7f02977db85b8d0899f0b67934a
rui95fer <rui95fer@gmail.com>
2026-01-08 23:05:31 +0000

    Creating the Profile Model

---
+++ app/Models/Profile.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Profile extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProfileFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'display_name',
+        'handle',
+        'bio',
+        'avatar_url',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
